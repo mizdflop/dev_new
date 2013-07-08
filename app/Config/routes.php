e3f76b5c0221a848fc17b5a91101885167adf8c0
Jay Greenspan <jayhuntsfish@gmail.com>
2013-07-08 10:17:51 -0400

    more

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -26,7 +26,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'users', 'action' => 'display', 'play_avp'));
+	Router::connect('/', array('controller' => 'users', 'action' => 'play_avp', 'play_avp'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
